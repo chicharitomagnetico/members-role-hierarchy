85d725513d3912b29c8f06fddc86a536308464de
Luis Moreno <chicharitomagnetico@gmail.com>
2020-05-05 17:41:55 -0500

    Update members-role-hierarchy-fork.php

--- members-role-hierarchy-fork.php
+++ members-role-hierarchy-fork.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Members - Role Hierarchy Fork
- * Plugin URI:  http://themehybrid.com/plugins/members
+ * Plugin URI:  https://github.com/chicharitomagnetico/members-role-hierarchy
  * Description: Role Hierarchy is an add-on plugin for Members for creating a hierarchical roles system. Its purpose is to make sure that users who can edit, create, delete, and promote users cannot do so to users above them in the hierarchy.
  * Version:     1.0.0
  * Author:      Justin Tadlock
